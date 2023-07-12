48bf9db5a5327b9736a3d22fe35bb605831084da
if21b193 <118160674+if21b193@users.noreply.github.com>
2023-07-12 14:32:30 +0200

    fixed an issue in the tourMapController

--- TourPlanner/src/main/java/com/example/tourplanner/UI/View/TourMapController.java
+++ TourPlanner/src/main/java/com/example/tourplanner/UI/View/TourMapController.java
@@ -2,8 +2,6 @@ package com.example.tourplanner.UI.View;
 
 
 import com.example.tourplanner.UI.ViewModel.TourMapViewModel;
-import javafx.beans.property.SimpleStringProperty;
-import javafx.beans.property.StringProperty;
 import javafx.fxml.FXML;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -11,8 +9,6 @@ import javafx.scene.image.ImageView;
 public class TourMapController {
 
     private final TourMapViewModel tourMapViewModel;
-    private StringProperty imageURL = new SimpleStringProperty();
-
     @FXML
     public Image mapImage;
     @FXML
@@ -23,13 +19,12 @@ public class TourMapController {
         }
 
     public void initialize() {
-        imageURL.bindBidirectional(tourMapViewModel.imageURLProperty());
-        imageURL.addListener((observable, oldValue, newValue) -> {
-            if (newValue != null) {
-                imageView.setImage(new Image(newValue));
+        tourMapViewModel.imageURLProperty().addListener(((observableValue, s, t1) -> {
+            if(t1 != null){
+                imageView.setImage(new Image(t1));
                 imageView.fitHeightProperty().bind(imageView.getScene().getWindow().heightProperty());
                 imageView.fitWidthProperty().bind(imageView.getScene().getWindow().widthProperty());
             }
-        });
+        }));
     }
 }
